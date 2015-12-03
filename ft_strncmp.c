444e394294744c3595624a371bb0539869bcf82a
Francois POLINI <fpolini@e3r4p9.42.fr>
2015-12-03 19:48:38 +0100

    correc strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/29 13:39:00 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/02 18:45:02 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/03 19:47:58 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && i < n)
-	{
 		i = i + 1;
-	}
+	if (i == n)
+		i = i - 1;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
