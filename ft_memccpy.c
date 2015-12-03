3ce5fd0b5b2eb8c1bd1a64cde334da26a938d815
Francois POLINI <fpolini@e3r4p9.42.fr>
2015-12-03 18:45:46 +0100

    ok

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 15:08:17 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/03 16:11:16 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/03 17:59:27 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	ch;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
+	s1 = dst;
+	s2 = (void*)src;
 	i = 0;
+	ch = c;
 	while (i < n)
 	{
-		if (((unsigned char*)src)[i] == (unsigned char)c)
-			return ((char*)&src[i + 1]);
+		if (s2[i] == ch)
+			return ((void*)&s2[i + 1]);
 		else
-			((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
+			s1[i] = s2[i];
 		i = i + 1;
 	}
 	return (NULL);
