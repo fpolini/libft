7700f13bb74e56a2ed7b6eb71bb2c5921e08e20d
Francois POLINI <fpolini@e3r4p9.42.fr>
2015-12-03 16:59:09 +0100

    ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 15:38:13 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/02 16:20:10 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/03 16:58:47 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 
