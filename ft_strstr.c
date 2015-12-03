24f8ef6f7734a7c57ad3870f317dd43101cbe8d5
Francois POLINI <fpolini@e3r4p9.42.fr>
2015-12-03 20:16:29 +0100

    strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/29 13:56:44 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/02 17:24:12 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/03 20:16:00 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,16 @@
 
 char		*ft_strstr(const char *s1, const char *s2)
 {
-	const char	*pointeur;
-	int			i;
+	char	*pointeur;
+	int		i;
 
-	pointeur = &s1[0];
+	pointeur = (char*)s1;
 	i = 0;
-	while (s1[i] != '\0')
+	while (ft_strlen(pointeur) > n)
 	{
-		if (ft_strncmp(pointeur, s2, ft_strlen(s2)) == 0)
-		{
-			return ((char*)pointeur);
-		}
-		else
-		{
-			i = i + 1;
-			pointeur = &s1[i];
-		}
+		pointeur = ft_strchr(s1, s2[0]);
+		if (ft_strnequ(pointeur, s2, n) == 0)
+			return (pointeur);
 	}
 	return (NULL);
 }
