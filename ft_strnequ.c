6802c9505776bb78cd9924ad33180179ab75de9c
Francois POLINI <fpolini@e3r4p9.42.fr>
2015-12-03 19:17:01 +0100

    ok

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 18:30:17 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/02 18:44:16 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/03 19:16:37 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && i < n)
+	if (n == 0)
+		return (1);
+	while (s1[i] == s2[i] && s1[i] != '\0' && i < n - 1)
 		i = i + 1;
 	if ((unsigned char)s1[i] - (unsigned char)s2[i] == 0)
 		return (1);
