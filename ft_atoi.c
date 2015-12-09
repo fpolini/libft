fd17aff751d395096686877231be4b874c728d5b
Francois POLINI <fpolini@e3r5p9.42.fr>
2015-12-09 12:50:59 +0100

    atoi pas bonne encore

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,28 +6,69 @@
 /*   By: fpolini <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 16:16:37 by fpolini           #+#    #+#             */
-/*   Updated: 2015/12/02 10:48:48 by fpolini          ###   ########.fr       */
+/*   Updated: 2015/12/09 12:50:22 by fpolini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+unsigned int ft_range(int i, const char *str)
+{
+	int	j;
+	int	t;
+	
+	t = 1;
+	j = i;
+	while (str[j] != '\0' && ft_isdigit(str[j]))
+	{
+		t = t * 10;
+		j = j + 1;
+	}
+	return (t / 10);
+}
+
+int	ft_print(int i, const char *str)
+{
+	while (str[i] != '\0' && 
+			(str[i] == '0' || 
+			 str[i] == ' ' || 
+			 str[i] == '\n' ||
+			 str[i] == '\t' ||
+			 str[i] == '\r' ||
+			 str[i] == '\v' ||
+			 str[i] == '\f'))
+		i = i + 1;
+	return (i);
+}
+
 int	ft_atoi(const char *str)
 {
-	int		i;
-	int		nbr;
-	int		p;
+	int				i;
+	unsigned int	nbr;
+	unsigned int	p;
+	int				a;
 
-	i = (int)ft_strlen(str) - 1;
-	p = 1;
+	i = 0;
 	nbr = 0;
-	while (i >= 0 && ft_isdigit(str[i]))
+	i = ft_print(i, str);
+	if (str[i] == '+' || str[i] == '-')
+		i = i + 1;
+	a = i - 1;
+	while (str[i] != '\0' && str[i] == '0')
+		i = i + 1;
+	p = ft_range(i, str);
+	while (str[i] != '\0')
 	{
-		nbr = nbr + ((str[i] - 48) * p);
-		i = i - 1;
-		p = p * 10;
+		if (!ft_isdigit(str[i]))
+			return (nbr);
+		else
+		{
+			nbr = nbr + (str[i] - 48) * p;
+			p = p / 10;
+			i = i + 1;
+		}
 	}
-	if (str[0] == '-')
+	if (str[a] == '-')
 		nbr = -nbr;
 	return (nbr);
 }
